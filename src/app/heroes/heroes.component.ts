065201dafd37777cdbad06324d2877c8379166f4
andemooca <andemooca@yahoo.com.br>
2019-03-26 00:25:39 -0300

    roteamento

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,39 +8,56 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-
-  //Lista de herois (mock)
-  // heroes = HEROES;
-
-  //Atribui as propriedades da classe heroi para selectedHero (hero selecionado)
-  selectedHero : Hero;
-
   heroes: Hero[];
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  //Exemplo de valores setados e enviados para a tela.
-  // hero: Hero = {
-  //   id : 1,
-  //   name : 'Windstorm'
-  // };
-
-  //Método que recebe um heroi ( click na tela )
-  onSelect(hero : Hero): void{
-    this.selectedHero = hero;
-  }
-
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
-
 }
+// export class HeroesComponent implements OnInit {
+
+//   //Lista de herois (mock)
+//   // heroes = HEROES;
+
+//   //Atribui as propriedades da classe heroi para selectedHero (hero selecionado)
+//   selectedHero : Hero;
+
+//   heroes: Hero[];
+//   constructor(private heroService: HeroService) { }
+
+//   ngOnInit() {
+//     this.getHeroes();
+//   }
+
+//   //Exemplo de valores setados e enviados para a tela.
+//   // hero: Hero = {
+//   //   id : 1,
+//   //   name : 'Windstorm'
+//   // };
+
+//   //Método que recebe um heroi ( click na tela )
+//   onSelect(hero : Hero): void{
+//     this.selectedHero = hero;
+//   }
+
+//   // getHeroes(): void {
+//   //   this.heroes = this.heroService.getHeroes();
+//   // }
+
+//   getHeroes(): void {
+//     this.heroService.getHeroes()
+//         .subscribe(heroes => this.heroes = heroes);
+//   }}
+
+
+
+
